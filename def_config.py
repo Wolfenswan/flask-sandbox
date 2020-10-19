a972b41df7dc0dda642d1929e2ed3d8b00cc34cd
Wolfenswan <fridaynightsarekillingme+github@gmail.com>
2020-10-19 16:16:36 +0200

    added a default config (mostly placeholder atm)

---
+++ def_config.py
@@ -0,0 +1,10 @@
+"""
+default config, move to instance.py
+TODO if possible, check in app.py if instance/config exists and if not copy/move this file
+"""
+
+import os
+import secrets
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_urlsafe(16)
