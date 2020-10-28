f3a79125778b3585a8d247ffc40c603d4de30092
Wolfenswan <fridaynightsarekillingme+github@gmail.com>
2020-10-28 20:28:15 +0100

    added new logos

--- projects/adx_abctrainer/adx_abctrainer.py
+++ projects/adx_abctrainer/adx_abctrainer.py
@@ -7,13 +7,14 @@ from flask import url_for
 from markupsafe import Markup
 
 def parse_word_to_html(word):
-    html_list = ['<div>']
+    """ Turns the given word into a list of img & div tags for the jinja-template """
+    html_list = ['<div class="adx-letters_row">']
     for i, letter in enumerate(list(word)):
         img_path = url_for('static', filename=f'adx_abctrainer/img/abc/{letter.lower()}.png')
-        html = f'<img src="{img_path}">'
+        html = f'<img src="{img_path}" class="adx-letter">'
         html_list.append(html)
-        if (i + 1) % 6 == 0:
-            html_list.append('</div>\n<div>')
+        if (i + 1) % 6 == 0: # add a new row for each six images
+            html_list.append('</div>\n<div class="adx_letters_row">')
     html_list.append('</div>')
     finalized_html = [Markup(tag) for tag in html_list]
     return finalized_html
