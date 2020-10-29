5ea260072780de4d5c444674354e85606386051b
Wolfenswan <fridaynightsarekillingme+github@gmail.com>
2020-10-29 14:52:54 +0100

    abc-trainer: more refactoring, improved error popups, added validation checks for certain German umlauts

--- projects/adx_abctrainer/form.py
+++ projects/adx_abctrainer/form.py
@@ -4,9 +4,10 @@ from wtforms.validators import DataRequired, Length, ValidationError
 
 
 class InputForm(FlaskForm):
-    word = StringField('Zauberwort eingeben:', validators=[DataRequired(message='Gib was ein!'),Length(max=18, message='Maximal 18 Buchstaben!')])
+    word = StringField('Zauberwort eingeben:', validators=[DataRequired(message='Gib was ein!'),Length(max=22, message='Maximal 18 Buchstaben!')])
     submit = SubmitField('Spuck was aus!')
 
     def validate_word(form, field):
-        if not field.data.isalpha():
-            raise ValidationError("Wörter bestehen üblicherweise nur aus Buchstaben...")
+        illegal_characters = ['ß','ä','ö','ü']
+        if not field.data.isalpha() or sum([char in field.data for char in illegal_characters])>0:
+            raise ValidationError("Nur Buchstaben von A-Z erlaubt!")
