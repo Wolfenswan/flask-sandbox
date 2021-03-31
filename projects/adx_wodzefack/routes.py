d814f11a8aaa13f56bc7abd1c8de647d605b92f7
Wolfenswan <fridaynightsarekillingme+github@gmail.com>
2021-03-31 13:16:52 +0200

    Updated index.html

--- projects/adx_wodzefack/routes.py
+++ projects/adx_wodzefack/routes.py
@@ -8,9 +8,12 @@ adx_wodzefack_bp = Blueprint('adx_wodzefack', __name__, template_folder='templat
 @adx_wodzefack_bp.route('/wod/', methods=["GET"])
 def wod():
     today = date.today()
-    return render_template("adx_wodzefack/wod.html", today=today.strftime("%d.%m.%Y") ,workout = adx_wodzefack.get_workout(today))
+    return redirect(url_for('.wod_specific', day=today.day, month=today.month))
 
 @adx_wodzefack_bp.route('/wod/<day>.<month>', methods=["GET"])
 def wod_specific(day, month):
-    today = datetime.strptime(f'{day}.{month}.2021', '%d.%m.%Y')
+    try:
+        today = datetime.strptime(f'{day}.{month}.2021', '%d.%m.%Y')
+    except:
+        today = datetime.strptime('01.01.2021', '%d.%m.%Y')
     return render_template("adx_wodzefack/wod.html", today=today.strftime("%d.%m.%Y") ,workout = adx_wodzefack.get_workout(today))
